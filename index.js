b2dbc35f1a333c9613ba7ca846bb40155ca51bd5
Thilrash Gowzul Ameen <36620465+Thilrash@users.noreply.github.com>
2021-11-18 00:48:03 +0530

    template frontend committed

---
+++ index.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+  // banner
+  $("#banner-area .owl-carousel").owlCarousel({
+    dots: true,
+    items: 1,
+  });
+
+  // top-sale
+  $("#top-sale .owl-carousel").owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+
+  // isotope filter
+  var $grid = $(".grid").isotope({
+    itemSelector: ".grid-item",
+    layoutMode: "fitRows",
+  });
+
+  // button click on filter
+  $(".button-group").on("click", "button", function () {
+    var filterValue = $(this).attr("data-filter");
+    $grid.isotope({ filter: filterValue });
+  });
+
+  // new arrivals carousel
+  $("#new-arrivals .owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+});
