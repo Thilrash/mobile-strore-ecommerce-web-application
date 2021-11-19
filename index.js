251b9764df0855bb6251517f3d153f0d4237b9d0
Thilrash Gowzul Ameen <36620465+Thilrash@users.noreply.github.com>
2021-11-19 13:41:55 +0530

    Template Frontend Commits

--- index.js
+++ index.js
@@ -71,10 +71,11 @@ $(document).ready(function () {
   // product qty section
   let $qty_up = $(".qty .qty-up");
   let $qty_down = $(".qty .qty-down");
-  let $input = $(".qty .qty_input");
+  // let $input = $(".qty .qty_input");
 
   // click
   $qty_up.click(function (e) {
+    let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
     if ($input.val() >= 1 && $input.val() <= 9) {
       $input.val(function (i, oldval) {
         return ++oldval;
@@ -83,6 +84,7 @@ $(document).ready(function () {
   });
 
   $qty_down.click(function (e) {
+    let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
     if ($input.val() > 1 && $input.val() <= 10) {
       $input.val(function (i, oldval) {
         return --oldval;
